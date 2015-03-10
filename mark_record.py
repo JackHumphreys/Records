f2e9db73c553b6e0a4e294d26abf8c84fa07c0f2
40618 <40618@longroad.ac.uk>
2015-03-10 12:18:43 +0000

    Mark Record

--- mark_record.py
+++ mark_record.py
@@ -11,34 +11,40 @@ class MarkRecord:
         self.total = None
         self.mean = None
 
-records = []
-for count in range(2):
-    record = MarkRecord()
-    record.first_name = input("Please enter your first name: ")
-    record.last_name = input("Please enter your last name: ")
-    record.module1 = int(input("Please enter marks for module 1: "))
-    record.module2 = int(input("Please enter marks for module 2: "))
-    record.module3 = int(input("Please enter marks for module 3: "))
-    record.module4 = int(input("Please enter marks for module 4: "))
-    record.total = (record.module1+record.module2+record.module3+record.module4)
-    records.append(record)
+def user_input():
+    records = []
+    for count in range(2):
+        record = MarkRecord()
+        record.first_name = input("Please enter your first name: ")
+        record.last_name = input("Please enter your last name: ")
+        record.module1 = int(input("Please enter marks for module 1: "))
+        record.module2 = int(input("Please enter marks for module 2: "))
+        record.module3 = int(input("Please enter marks for module 3: "))
+        record.module4 = int(input("Please enter marks for module 4: "))
+        record.total = (record.module1+record.module2+record.module3+record.module4)
+        records.append(record)
+    return record, records
 
 
 #Mean Total
-total = 0
-for count in range(2):
-    total = total + records[count].total
-mean = total/len(records)-1
+def mean(tempRecord, finalRecord):
+    total = 0
+    for count in range(2):
+        total = total + finalRecord[count].total
+    mean = total/len(finalRecord)-1
+    return mean
 
     
 
 #Main
+tempRecord, finalRecord = user_input()
+average = mean(tempRecord, finalRecord)
 print("_"*40)
 print("|{0:>18}|{1:>2}|{2:>2}|{3:>2}|{4:>2}|{5:>3}|{6:>5}".format("Name","M1","M2","M3","M4","Total","Mean"))
-for record in records:
+for tempRecord in finalRecord:
     for count in range(2):
-        meanCompare = records[count].total - mean
-    print("|{0:>5} {1:>12}|{2:>2}|{3:>2}|{4:>2}|{5:>2}|{6:>5}|{7:>5}".format(record.first_name, record.last_name, record.module1, record.module2, record.module3, record.module4, record.total, meanCompare))
+        meanCompare = finalRecord[count].total - average
+    print("|{0:>5} {1:>12}|{2:>2}|{3:>2}|{4:>2}|{5:>2}|{6:>5}|{7:>5}".format(tempRecord.first_name, tempRecord.last_name, tempRecord.module1, tempRecord.module2, tempRecord.module3, tempRecord.module4, tempRecord.total, meanCompare))
     print("-"*40)
 
 
