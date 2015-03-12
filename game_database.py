9b182c56baed18d987046282d6332f608001ffed
Jack Humphreys <40618@longroad.ac.uk>
2015-03-12 20:19:10 +0000

    Game Database

--- game_database.py
+++ game_database.py
@@ -1,3 +1,5 @@
+import pickle
+
 class GameData:
     def __init__(self):
         self.name = None
@@ -8,21 +10,21 @@ class GameData:
         self.online = None
 
 def load_games():
-    with open("\git\Files\gamefile.txt", mode = "r", encoding = "utf-8") as gamefile:
-        readRecord = gamefile.read()
-        return readRecord            
+    with open(r"\Users\Jumphreys\git\Records\gamefile.txt", mode = "rb", encoding = "utf-8") as gamefile:
+        data = gamefile.read()
+        return data            
             
 
 def save_games(games, record):
-    with open("\git\Files\gamefile.txt", mode = "w", encoding = "utf-8") as gamefile:
+    with open(r"\Users\Jumphreys\git\Records\gamefile.txt", mode = "wb", encoding = "utf-8") as gamefile:
         for record in games:
-            gamefile.write(record.name + "\n")
-            gamefile.write(record.platform + "\n")
-            gamefile.write(record.genre + "\n")
-            gamefile.write(record.cost + "\n")
-            gamefile.write(record.players + "\n")
-            gamefile.write(record.online + "\n")
-            print()
+            pickle.dump(record.name, gamefile)
+            pickle.dump(record.platform, gamefile)
+            pickle.dump(record.genre, gamefile)
+            pickle.dump(record.cost, gamefile)
+            pickle.dump(record.players, gamefile)
+            pickle.dump(record.online, gamefile)
+
         
         
 
@@ -31,15 +33,17 @@ def save_games(games, record):
 def display_games(games, record):
     print()
     print()
-    print("|{0:^5}|{1:^5}|{2:^5}|{3:^5}|{4:^5}|{5:^5}|".format("Name", "Platform", "Genre", "Cost", "Players", "Online"))
+    print("|{0:^20}|{1:^5}|{2:^5}|{3:^6}|{4:^5}|{5:^5}|".format("Name", "Platform", "Genre", "Cost", "Players", "Online"))
+    print("-"*59)
     for record in games:
-        print("|{0:^5}|{1:^8}|{2:^5}|{3:^5}|{4:^7}|{5:^6}|".format(record.name, record.platform, record.genre, record.cost, record.players, record.online))
-
+        print("|{0:^20}|{1:^8}|{2:^5}|{3:^5}|{4:^7}|{5:^6}|".format(record.name, record.platform, record.genre, record.cost, record.players, record.online))
+        print("-"*59)
+        
 def get_game_from_user():
     gameslist = []
-    tempRecord = GameData()
     finished = False
     while not finished:
+        tempRecord = GameData()
         name = input("Name of game (-1 to finish): ")
         if name == "-1":
             finished = True
